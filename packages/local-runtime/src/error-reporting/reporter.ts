MiniMax-AI/minimax-code

fix: require separate consent for automatic telemetry uploads
Default runtime metrics and diagnostics to off, honor global opt-outs,
and recheck diagnostic consent before sending. Include privacy
regressions and the generated source and test inventories.

packages/local-runtime/src/error-reporting/reporter.ts
@@ -15,7 +15,7 @@
  * every stage; never expose them to callers or affect LLM requests, retries, or turn results.
  */
 
-import { getRuntimeBuildEnv, getRuntimeRegion } from '@mavis/config';
+import { getRuntimeBuildEnv, getRuntimeRegion, isTelemetryChannelEnabled } from '@mavis/config';
 import { LLM_ERROR_REASONS } from '@mavis/shared/llm-error-classifier';
 
 import { logger } from '../common/logger.js';
@@ -62,6 +62,7 @@ export function createDesktopErrorReporter(
 
   /** Read login state, encrypt each event_log, and POST the batch without letting failures affect the main flow. */
   async function encryptAndSend(events: DesktopErrorLog[]): Promise<void> {
+    if (!isTelemetryChannelEnabled('diagnostics', options.readTelemetryEnabled)) return;
     const authContext = options.authContextGetter?.();
     const token = authContext?.accessToken?.trim();
     const userId = authContext?.realUserID?.trim();
@@ -103,6 +104,7 @@ export function createDesktopErrorReporter(
   return {
     report(event: DesktopErrorLog): void {
       try {
+        if (!isTelemetryChannelEnabled('diagnostics', options.readTelemetryEnabled)) return;
         // Drop oversized raw logs entirely, never truncate (design §2). Measure plaintext size to bound both
         // memory usage and the final request body.
         const byteLength = Buffer.byteLength(event.event_log, 'utf8');

packages/local-runtime/src/runtime/host-metrics.ts
@@ -1,4 +1,9 @@
-import { getRuntimeBuildEnv, getRuntimeRegion, isManagedRuntime } from '@mavis/config';
+import {
+  getRuntimeBuildEnv,
+  getRuntimeRegion,
+  isManagedRuntime,
+  isTelemetryChannelEnabled,
+} from '@mavis/config';
 import { createPiTurnHistogramBucketsByName } from '@mavis/agent-core/pi-turn-runner';
 
 import { logger } from '../common/logger.js';
@@ -12,6 +17,8 @@ import {
 import type { LocalRuntimeMode } from './mode.js';
 
 export interface LocalRuntimeHostMetricsOptions {
+  /** Explicit `telemetry.metrics` opt-in; environment opt-outs always take precedence. */
+  readonly readTelemetryEnabled?: () => boolean | undefined;
   readonly runtimeOwnerKind: string;
   readonly runtimeMode?: LocalRuntimeMode;
   readonly appVersion?: string;
@@ -101,7 +108,9 @@ export function buildLocalRuntimeMetricsClient(
   options: LocalRuntimeHostMetricsOptions,
 ): MetricsClient {
   const managed = isManagedRuntime();
-  const reporter = options.metricsReporter ?? buildManagedReporter(managed);
+  // The built-in cloud transport additionally requires an explicit `telemetry.metrics` opt-in.
+  const cloudEnabled = managed && isTelemetryChannelEnabled('metrics', options.readTelemetryEnabled);
+  const reporter = options.metricsReporter ?? buildManagedReporter(cloudEnabled);
 
   const onError = (err: unknown): void => {
     logger.warn(
@@ -110,8 +119,11 @@ export function buildLocalRuntimeMetricsClient(
     );
   };
 
-  if (!managed && !options.metricsReporter) {
-    logger.info({ reason: 'unmanaged_runtime' }, 'Local runtime metrics reporter disabled');
+  if (!cloudEnabled && !options.metricsReporter) {
+    logger.info(
+      { reason: managed ? 'telemetry_metrics_opt_in_required' : 'unmanaged_runtime' },
+      'Local runtime metrics reporter disabled',
+    );
   }
 
   return createLocalRuntimeMetricsClient({

packages/tui/src/observability/incident-reporter.ts
@@ -12,7 +12,7 @@ import {
 import { arch, platform } from 'node:os';
 import { basename, dirname, join } from 'node:path';
 
-import type { MavisBuildEnv, MavisRegion } from '@mavis/config';
+import { isTelemetryChannelEnabled, type MavisBuildEnv, type MavisRegion } from '@mavis/config';
 
 export type TuiIncidentPhase = 'startup' | 'runtime' | 'shutdown';
 export type TuiIncidentSeverity = 'fatal' | 'error' | 'warning';
@@ -66,6 +66,8 @@ export interface TuiIncidentAuthContext {
 }
 
 export interface CreateTuiIncidentReporterOptions {
+  /** Explicit `telemetry.diagnostics` opt-in; environment opt-outs always take precedence. */
+  readonly readTelemetryEnabled?: () => boolean | undefined;
   readonly dataDir: string;
   readonly appVersion: string;
   readonly region: MavisRegion;
@@ -313,7 +315,10 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
       };
       writeJsonAtomically(
         this.directory,
-        join(this.directory, `pending-${occurredAtMs}-${incidentId}.json`),
+        join(
+          this.directory,
+          `${this.uploadEnabled() ? 'pending' : 'local'}-${occurredAtMs}-${incidentId}.json`,
+        ),
         stored,
       );
       this.prune();
@@ -379,6 +384,7 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
   }
 
   private async drainPending(): Promise<void> {
+    if (!this.uploadEnabled()) return;
     const resolveAuthContext = this.options.resolveAuthContext;
     if (!resolveAuthContext) return;
     let auth: TuiIncidentAuthContext | undefined;
@@ -415,7 +421,7 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
     accessToken: string,
     realUserID: string,
   ): Promise<boolean> {
-    if (this.networkDrainStopped) return false;
+    if (this.networkDrainStopped || !this.uploadEnabled()) return false;
     const requestController = new AbortController();
     const requestTimeout = setTimeout(
       () => requestController.abort(),
@@ -502,10 +508,14 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
     }
   }
 
+  private uploadEnabled(): boolean {
+    return isTelemetryChannelEnabled('diagnostics', this.options.readTelemetryEnabled);
+  }
+
   private prune(): void {
     const nowMs = this.nowMs();
     const incidentFiles = safeReadDirectory(this.directory)
-      .filter((entry) => /^(?:pending|sent)-.*\.json$/u.test(entry))
+      .filter((entry) => /^(?:pending|sent|local)-.*\.json$/u.test(entry))
       .flatMap((entry) => {
         const path = join(this.directory, entry);
         try {
@@ -527,6 +537,7 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
     }
     const retained = incidentFiles.filter((file) => nowMs - file.modifiedAtMs <= RETENTION_MS);
     const deletionOrder = [
+      ...retained.filter((file) => basename(file.path).startsWith('local-')),
       ...retained.filter((file) => basename(file.path).startsWith('sent-')),
       ...retained.filter((file) => basename(file.path).startsWith('pending-')),
     ];

AkashaCorporation/Bari

packages/local-runtime/src/error-reporting/reporter.ts
@@ -15,7 +15,7 @@
  * every stage; never expose them to callers or affect LLM requests, retries, or turn results.
  */
 
-import { getRuntimeBuildEnv, getRuntimeRegion } from '@bari/config';
+import { getRuntimeBuildEnv, getRuntimeRegion, isTelemetryChannelEnabled } from '@bari/config';
 import { LLM_ERROR_REASONS } from '@bari/shared/llm-error-classifier';
 
 import { logger } from '../common/logger.js';
@@ -62,6 +62,7 @@ export function createDesktopErrorReporter(
 
   /** Read login state, encrypt each event_log, and POST the batch without letting failures affect the main flow. */
   async function encryptAndSend(events: DesktopErrorLog[]): Promise<void> {
+    if (!isTelemetryChannelEnabled('diagnostics', options.readTelemetryEnabled)) return;
     const authContext = options.authContextGetter?.();
     const token = authContext?.accessToken?.trim();
     const userId = authContext?.realUserID?.trim();
@@ -103,6 +104,7 @@ export function createDesktopErrorReporter(
   return {
     report(event: DesktopErrorLog): void {
       try {
+        if (!isTelemetryChannelEnabled('diagnostics', options.readTelemetryEnabled)) return;
         // Drop oversized raw logs entirely, never truncate (design §2). Measure plaintext size to bound both
         // memory usage and the final request body.
         const byteLength = Buffer.byteLength(event.event_log, 'utf8');

packages/local-runtime/src/runtime/host-metrics.ts
@@ -1,4 +1,9 @@
-import { getRuntimeBuildEnv, getRuntimeRegion, isManagedRuntime } from '@bari/config';
+import {
+  getRuntimeBuildEnv,
+  getRuntimeRegion,
+  isManagedRuntime,
+  isTelemetryChannelEnabled,
+} from '@bari/config';
 import { createPiTurnHistogramBucketsByName } from '@bari/agent-core/pi-turn-runner';
 
 import { logger } from '../common/logger.js';
@@ -12,6 +17,8 @@ import {
 import type { LocalRuntimeMode } from './mode.js';
 
 export interface LocalRuntimeHostMetricsOptions {
+  /** Explicit `telemetry.metrics` opt-in; environment opt-outs always take precedence. */
+  readonly readTelemetryEnabled?: () => boolean | undefined;
   readonly runtimeOwnerKind: string;
   readonly runtimeMode?: LocalRuntimeMode;
   readonly appVersion?: string;
@@ -101,7 +108,9 @@ export function buildLocalRuntimeMetricsClient(
   options: LocalRuntimeHostMetricsOptions,
 ): MetricsClient {
   const managed = isManagedRuntime();
-  const reporter = options.metricsReporter ?? buildManagedReporter(managed);
+  // The built-in cloud transport additionally requires an explicit `telemetry.metrics` opt-in.
+  const cloudEnabled = managed && isTelemetryChannelEnabled('metrics', options.readTelemetryEnabled);
+  const reporter = options.metricsReporter ?? buildManagedReporter(cloudEnabled);
 
   const onError = (err: unknown): void => {
     logger.warn(
@@ -110,8 +119,11 @@ export function buildLocalRuntimeMetricsClient(
     );
   };
 
-  if (!managed && !options.metricsReporter) {
-    logger.info({ reason: 'unmanaged_runtime' }, 'Local runtime metrics reporter disabled');
+  if (!cloudEnabled && !options.metricsReporter) {
+    logger.info(
+      { reason: managed ? 'telemetry_metrics_opt_in_required' : 'unmanaged_runtime' },
+      'Local runtime metrics reporter disabled',
+    );
   }
 
   return createLocalRuntimeMetricsClient({

packages/tui/src/observability/incident-reporter.ts
@@ -12,7 +12,7 @@ import {
 import { arch, platform } from 'node:os';
 import { basename, dirname, join } from 'node:path';
 
-import type { MavisBuildEnv, MavisRegion } from '@bari/config';
+import { isTelemetryChannelEnabled, type MavisBuildEnv, type MavisRegion } from '@bari/config';
 
 export type TuiIncidentPhase = 'startup' | 'runtime' | 'shutdown';
 export type TuiIncidentSeverity = 'fatal' | 'error' | 'warning';
@@ -66,6 +66,8 @@ export interface TuiIncidentAuthContext {
 }
 
 export interface CreateTuiIncidentReporterOptions {
+  /** Explicit `telemetry.diagnostics` opt-in; environment opt-outs always take precedence. */
+  readonly readTelemetryEnabled?: () => boolean | undefined;
   readonly dataDir: string;
   readonly appVersion: string;
   readonly region: MavisRegion;
@@ -313,7 +315,10 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
       };
       writeJsonAtomically(
         this.directory,
-        join(this.directory, `pending-${occurredAtMs}-${incidentId}.json`),
+        join(
+          this.directory,
+          `${this.uploadEnabled() ? 'pending' : 'local'}-${occurredAtMs}-${incidentId}.json`,
+        ),
         stored,
       );
       this.prune();
@@ -379,6 +384,7 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
   }
 
   private async drainPending(): Promise<void> {
+    if (!this.uploadEnabled()) return;
     const resolveAuthContext = this.options.resolveAuthContext;
     if (!resolveAuthContext) return;
     let auth: TuiIncidentAuthContext | undefined;
@@ -415,7 +421,7 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
     accessToken: string,
     realUserID: string,
   ): Promise<boolean> {
-    if (this.networkDrainStopped) return false;
+    if (this.networkDrainStopped || !this.uploadEnabled()) return false;
     const requestController = new AbortController();
     const requestTimeout = setTimeout(
       () => requestController.abort(),
@@ -502,10 +508,14 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
     }
   }
 
+  private uploadEnabled(): boolean {
+    return isTelemetryChannelEnabled('diagnostics', this.options.readTelemetryEnabled);
+  }
+
   private prune(): void {
     const nowMs = this.nowMs();
     const incidentFiles = safeReadDirectory(this.directory)
-      .filter((entry) => /^(?:pending|sent)-.*\.json$/u.test(entry))
+      .filter((entry) => /^(?:pending|sent|local)-.*\.json$/u.test(entry))
       .flatMap((entry) => {
         const path = join(this.directory, entry);
         try {
@@ -527,6 +537,7 @@ class LocalTuiIncidentReporter implements TuiIncidentReporter {
     }
     const retained = incidentFiles.filter((file) => nowMs - file.modifiedAtMs <= RETENTION_MS);
     const deletionOrder = [
+      ...retained.filter((file) => basename(file.path).startsWith('local-')),
       ...retained.filter((file) => basename(file.path).startsWith('sent-')),
       ...retained.filter((file) => basename(file.path).startsWith('pending-')),
     ];